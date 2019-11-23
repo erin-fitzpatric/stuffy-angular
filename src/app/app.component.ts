7f735d2c23745bb9ad0267e3534aba0c12f7a62b
erin-fitzpatric <efitzpatric@gmail.com>
2019-11-23 13:19:52 -0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Stuffy } from './model/stuffy.class';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'stuffy';
+  title: string = 'Stuffies';
+  header: string = 'New Stuffy';
+
+  stuffies: Stuffy[] = [];
+  // two way bindings
+  id: number = null;
+  type: string = "";
+  color: string = "";
+  size: string = "";
+  limbs: number = null;
+
+  addStuffy(): void {
+    let s: Stuffy = new Stuffy(this.id, this.type, this.color, this.size, this.limbs);
+    this.stuffies.push(s);
+  }
 }
---
+++ src/app/model/stuffy.class.ts
@@ -0,0 +1,17 @@
+import { NumberSymbol } from '@angular/common';
+
+export class Stuffy {
+    id: number;
+    type: string;
+    color: string;
+    size: string;
+    limbs: number;
+
+    constructor(id: number, type: string, color: string, size: string, limbs: number) {
+        this.id = id;
+        this.type = type;
+        this.color= color;
+        this.size = size;
+        this.limbs= limbs;
+    }
+}
